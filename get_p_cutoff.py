12b3326a0a5743332c1cd30dd7f81fa47d27db71
berilerdogdu <40407606+berilerdogdu@users.noreply.github.com>
2023-04-25 22:38:01 -0400

    Update get_p_cutoff.py

--- get_p_cutoff.py
+++ get_p_cutoff.py
@@ -2,14 +2,13 @@ import sys, getopt
 import numpy as np
 import pandas as pd
 
-
 def get_p_cutoff(p_values, k):
-
+    n = len(p_values)
+    t = int(k*n)
     p_values = np.array(p_values)
-    p_cutoff = np.sort(p_values)[k-1]
+    p_cutoff = np.sort(p_values)[t-1]
 
     return p_cutoff
-
 
 def main(argv):
 
@@ -26,13 +25,13 @@ def main(argv):
             print("Usage: python get_p_cutoff.py -k <K> -p <spit test p_values file>")
             sys.exit()
         elif opt in ("-k", "--K"):
-            k = int(arg)
+            k = float(arg)
         elif opt in ("-p", "--spit_test_p_values_file"):
             p_values_file = arg
     
     p_values = pd.read_csv(p_values_file, names = ['p']).p.to_list()
     p_cutoff = get_p_cutoff(p_values, k)
-    print( p_cutoff)
+    print(p_cutoff)
 
 
 
