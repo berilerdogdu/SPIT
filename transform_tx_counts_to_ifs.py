38829d7522acf0329d8948a4756613cc60dd25d8
berilerdogdu <40407606+berilerdogdu@users.noreply.github.com>
2023-07-01 01:47:47 -0400

    Update transform_tx_counts_to_ifs.py

--- transform_tx_counts_to_ifs.py
+++ transform_tx_counts_to_ifs.py
@@ -1,4 +1,5 @@
-import sys, getopt
+import sys
+import argparse
 import numpy as np
 import pandas as pd
 import random
@@ -6,8 +7,7 @@ import math
 from collections import defaultdict
 
 
-def convert_counts_to_IF_and_gene_level(counts):
-    
+def convert_counts_to_IF_and_gene_level(counts): 
     counts_w_genes = counts
     gene_level_counts = counts_w_genes.groupby('gene_id').sum()
     gene_level_counts = gene_level_counts + 0.00001
@@ -18,43 +18,29 @@ def convert_counts_to_IF_and_gene_level(counts):
 
 def main(argv):
 
-    tx_counts_file = ''
-    tx2gene_file = ''
-    iso_fracs_out = ''
-    gene_counts_out = ''
+    parser = argparse.ArgumentParser()
+    parser._optionals.title = 'Command-line arguments:'
+    parser.add_argument('-i', metavar='tx_counts.txt', required=True, type=str, help='Transcript level counts file (tsv)')
+    parser.add_argument('-m', metavar='tx2gene.txt', required=True, type=str, help='Transcript to gene mapping file (tsv)')
+    parser.add_argument('-F', metavar='filtered_ifs.txt', required=True, type=str, help='Output file path for isoform fractions (IFs)')
+    parser.add_argument('-G', metavar='filtered_gene_counts.txt', required=True, type=str, help='Output file path for gene counts')
+    parser.add_argument('-w', '--write', action='store_true', help='Write the number of transcripts & genes left after filtering all-zeroes to stdout.')
     
-    try:
-        opts, args = getopt.getopt(argv,"hi:m:F:G:",["tfile=", "tx2gene_file=", "iso_fracs_out=", "gene_counts_out="])
-    except getopt.GetoptError:
-        print("Usage: transform_tx_counts_to_ifs.py -i  <transcript level counts file (tsv)> -m <transcript to gene mapping file (tsv)> -F <output file path for filtered fractions> -G <output file path for filtered gene counts>")
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print("Usage: transform_tx_counts_to_ifs.py -i  <transcript level counts file (tsv)> -m <transcript to gene mapping file (tsv)> -F <output file path for filtered fractions> -G <output file path for filtered gene counts>")
-            sys.exit()
-        elif opt in ("-i", "--tfile"):
-            tx_counts_file = arg
-        elif opt in ("-m", "--tx2gene_file"):
-            tx2gene_file = arg
-        elif opt in ("-f", "--iso_fracs_out"):
-            iso_fracs_out = arg
-        elif opt in ("-g", "--gene_counts_out"):
-            gene_counts_out = arg
-    
-    tx_count_data = pd.read_csv(tx_counts_file, sep='\t', index_col='feature_id')
-    tx2gene = pd.read_csv(tx2gene_file, sep = '\t')
+    args = parser.parse_args()    
+    tx_count_data = pd.read_csv(args.i, sep='\t', index_col='feature_id')
+    tx2gene = pd.read_csv(args.m, sep = '\t')
     txs_w_genes = tx_count_data.join(tx2gene.set_index('tx_id'))
-    print("Input number of transcripts: ", tx_count_data.shape[0])
-    print("Input number of genes: ", len(tx_count_data.gene_id.unique()))
-    
+    if(args.write):
+        print("Input number of transcripts: ", tx_count_data.shape[0])
+        print("Input number of genes: ", len(tx_count_data.gene_id.unique()))
     tx_count_data = tx_count_data[tx_count_data.values.sum(axis=1) != 0]
-    print("After filtering out all-zeros:")
-    print("\tNumber of transcripts", len(tx_count_data.index))
-    print("\tNumber of genes: ", len(tx_count_data_w_genes.gene_id.unique()))
-
+    if(args.write):
+        print("After filtering out all-zeros:")
+        print("\tNumber of transcripts", len(tx_count_data.index))
+        print("\tNumber of genes: ", len(tx_count_data_w_genes.gene_id.unique()))
     IFs, gene_level_counts = convert_counts_to_IF_and_gene_level(tx_count_data)
-    IFs.to_csv(iso_fracs_out, sep = '\t')
-    gene_level_counts.to_csv(gene_counts_out, sep = '\t')
+    IFs.to_csv(args.F, sep = '\t')
+    gene_level_counts.to_csv(args.G, sep = '\t')
 
 
 if __name__ == "__main__":
